f001c9d7275cab6f7592c5dda13b9c8765a9967f
Isabelle Delmas <isabelle@delmas.us>
2023-02-05 22:32:42 -0800

    bugfixing

--- REST_API/src/main/java/UCB/MICS/InHome/Utilities.java
+++ REST_API/src/main/java/UCB/MICS/InHome/Utilities.java
@@ -6,16 +6,15 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Map;
 
-public class JsonUtilities {
+public class Utilities {
     public static Map<String, String> getFromRequest(HttpServletRequest req) throws IOException {
-        byte[] bytes = new byte[req.getInputStream().available()];
-        return new ObjectMapper().readValue(bytes, Map.class);
+        return new ObjectMapper().readValue(req.getInputStream().readAllBytes(), Map.class);
     }
 
     public static byte[] ipToByteArray(String ip) throws NumberFormatException
     {
         byte[] ipInBytes = new byte[8];
-        String[] elements = ip.split(":");
+        String[] elements = ip.split("\\.");
         try {
             for(int i = 0; i < 4; i++) {
                 ipInBytes[i*2] = (byte)(Integer.parseInt(elements[i]) >>> 8);
--- REST_API/src/main/java/UCB/MICS/InHome/servlet/AdminServlet.java
+++ REST_API/src/main/java/UCB/MICS/InHome/servlet/AdminServlet.java
@@ -1,6 +1,7 @@
 package UCB.MICS.InHome.servlet;
 
-import UCB.MICS.InHome.JsonUtilities;
+import UCB.MICS.InHome.Utilities;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 
 import javax.inject.Singleton;
 import javax.servlet.ServletException;
@@ -20,17 +21,37 @@ public class AdminServlet extends HttpServlet {
     Logger logger = Logger.getLogger("AdminServlet");
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException
+            throws IOException
     {
-        Map<String, String> json = JsonUtilities.getFromRequest(req);
+        Map<String, String> json = null;
+        try {
+            json = Utilities.getFromRequest(req);
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "malformed json");
+        }
         String mac = json.get("mac");
         String name = json.get("name");
         String ip = json.get("ip");
         if(isNullOrEmpty(mac)||isNullOrEmpty(name)||isNullOrEmpty(ip)){
-            // return error request malformed
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "name, mac, or ip missing");
         }
-        Long longMac = Long.parseLong(mac, 16);
-        byte[] ipB = JsonUtilities.ipToByteArray(ip);
+        try {
+            byte[] ipB = Utilities.ipToByteArray(ip);
+        }
+        catch (NumberFormatException e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "IP malformed");
+        }
+        try {
+            byte[] macB = Utilities.macToByteArray(mac);
+        }
+        catch(NumberFormatException e){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "MAC address malformed");
+        }
+
+        // now save info to database
+
+        // now update database revision
+
 
         logger.log(Level.INFO, "post was called");
     }
