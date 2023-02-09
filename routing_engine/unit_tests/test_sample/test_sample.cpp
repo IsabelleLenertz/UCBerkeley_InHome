52550881a76837df95279fadae338c2f6f0ba60f
Andrew <asrobertson@ischool.berkeley.edu>
2023-02-08 21:56:46 -0800

    AR - Adding Unit Test Makefile and sample test case source file

---
+++ routing_engine/unit_tests/test_sample/test_sample.cpp
@@ -0,0 +1,13 @@
+// This is a sample unit test file
+// All unit test files must be within
+// the unit_tests directory and have
+// a name which matches the pattern
+// test_*.cpp
+
+#include "gtest/gtest.h"
+
+// Trivial test case
+TEST(my_test_group, my_test)
+{
+    ASSERT_EQ(true, true);
+}
