740555172e182f6688027071ced675c6f613c893
Isabelle Delmas <isabelle@delmas.us>
2023-02-20 11:48:47 -0800

    added db queries

--- REST_API/src/main/java/UCB/MICS/InHome/jdbc/queries.java
+++ REST_API/src/main/java/UCB/MICS/InHome/jdbc/queries.java
@@ -7,5 +7,9 @@ public abstract class queries {
     public static String UPDATE_NAME = "UPDATE devices SET Name = ? WHERE Name = ?";
     public static String SELECT_ALL_DEVICES = "SELECT * FROM devices";
     public static String GET_ONE_DEVICE = "SELECT * FROM devices WHERE mac = ? LIMIT 1";
-
+    public static String GET_ALL_POLICIES = "SELECT * FROM policies";
+    public static String GET_POLICY_BY_DEVICE_FROM = "SELECT policyId FROM policies WHERE deviceFrom = ?";
+    public static String GET_POLICY_BY_DEVICE_TO = "SELECT policyId FROM policies WHERE deviceTo = ?";
+    public static String GET_POLICY_BY_DEVICE_ANY = "SELECT policyIg FROM policies WHERE deviceTo = ? OR deviceFrom = ?";
+    public static String DELETE_POLICIES_BY_ID = "DELETE FROM policies WHERE policyId = ?";
 }
