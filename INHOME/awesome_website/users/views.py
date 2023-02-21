ac97d9f7b6860330c09f665d548510d951c5e846
Joseph Giulianelli <104740569+jjg120@users.noreply.github.com>
2023-02-20 16:33:36 -0800

    INHOME update
    INHOME update

--- INHOME/awesome_website/users/views.py
+++ INHOME/awesome_website/users/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def dashboard(request):
     return render(request, "users/dashboard.html")
 
 def INHOME(request):
     return render(request, "users/INHOME.html")
-
-
 
 
