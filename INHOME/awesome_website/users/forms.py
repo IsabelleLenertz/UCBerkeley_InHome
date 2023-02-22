3448398e6a5e604e45a2150f144823ecf539120e
Joseph Giulianelli <104740569+jjg120@users.noreply.github.com>
2023-02-21 17:55:27 -0800

    setting up for calling script
    setting up for calling script

--- INHOME/awesome_website/users/forms.py
+++ INHOME/awesome_website/users/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 
 class DeviceForm(forms.Form):
-	MAC = forms.CharField(label="MAC address", max_length=17)
-	IPv4_addr = forms.CharField(label="IPv4 Address", max_lenth=15)
-        device_name = form.Charfield(label="Device name", max_length=30)
+    MAC = forms.CharField(label="MAC address", max_length=17)
+    IPv4_addr = forms.CharField(label="IPv4 Address", max_length=15)
+    device_name = forms.CharField(label="Device name", max_length=30)
--- INHOME/awesome_website/users/views.py
+++ INHOME/awesome_website/users/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from users.forms import DeviceForm
+from django.views.generic.edit import FormView
 
 
 # Create your views here.
@@ -8,4 +10,14 @@ def dashboard(request):
 def INHOME(request):
     return render(request, "users/INHOME.html")
 
+class DeviceFormView(FormView):
+    template_name = 'INHOME.html'
+    form_class = DeviceForm
+    success_url = '/'
 
+def form_valid(self, form):
+    MAC = self.form.cleaned_data["MAC"]
+    IPv4_addr = self.form.cleaned_data["IPv4_addr"]
+    device_name = self.form.cleaned_data["device_name"]
+    sendDataToServer("url", MAC,  IPVv4_addr, device_name)
+    return super().form_valid(form)
