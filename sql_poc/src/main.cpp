6815fab9f7bcf1e7ca599d2a2fb435dff9c614ef
root <root@DESKTOP-6LVO0I6>
2023-03-28 12:21:58 -0700

    updated poc with mysqlx lib

--- sql_poc/src/main.cpp
+++ sql_poc/src/main.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
+#include <typeinfo>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "../../routing_engine/inc/logging/Logger.hpp"
+#include <net/ethernet.h>
+
 /* uncomment for applications that use vectors */
 /*#include <vector>*/
 
@@ -13,6 +19,9 @@
 #define EXAMPLE_USER "root"
 #define EXAMPLE_PASS "password" //my-secret-pw or password
 #define EXAMPLE_DB "InHome"
+#define MAC 1
+#define IPv4 3
+#define NAME 0
 
 using namespace std;
 
@@ -25,6 +34,18 @@ struct Device
   uint32_t ipv4;
 };
 
+template <typename IntegerType>
+IntegerType bitsToInt( IntegerType& result, const unsigned char* bits, bool little_endian = true )
+{
+result = 0;
+if (little_endian)
+  for (int n = sizeof( result ); n >= 0; n--)
+    result = (result << 8) +bits[ n ];
+else
+  for (unsigned n = 0; n < sizeof( result ); n++)
+    result = (result << 8) +bits[ n ];
+return result;
+}
 
 int main(int argc, const char **argv)
 {
@@ -46,9 +67,27 @@ int main(int argc, const char **argv)
   mysqlx::Table myTable = myDb.getTable("devices");
   mysqlx::RowResult res = myTable.select("*").execute();
   list<mysqlx::Row> rows = res.fetchAll();
-  for(mysqlx::Row row : rows) {
-    cout <<  row.getBytes(0);
+  for(auto& row : rows) {
+    cout << row.get(NAME) << endl;
+    sockaddr_in sock;  
+    sock.sin_family = AF_INET;
+    sock.sin_port = htons(0);
+
+    // Ensure format of the table data.
+    if (row.getBytes(IPv4).size() < sizeof(sock.sin_addr.s_addr)) continue;
+    memcpy(&sock.sin_addr.s_addr, row.getBytes(3).begin(), sizeof(sock.sin_addr.s_addr));
+
+    ether_addr eth;
+    if (row.getBytes(MAC).size() < sizeof(eth.ether_addr_octet)) continue;
+    memcpy(eth.ether_addr_octet, row.getBytes(1).begin(), sizeof(eth.ether_addr_octet));
+
+    std::cout << Logger::IPToString(reinterpret_cast<sockaddr&>(sock)) << std::endl;
   }
   cout << "Done." << endl;
+
+  // Query the latest revision id
+  res = myDb.getTable("revisions").select("revisionId").orderBy("revisionDate DESC").limit(1).execute();
+  mysqlx::Row row = res.fetchOne(); 
+  cout << endl << endl << row.get(0) << endl;
   return EXIT_SUCCESS;
 }
